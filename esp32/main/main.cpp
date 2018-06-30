2437481befd2075f6f3f1797a08f3419df7c3619
Torsten Martinsen <torsten@bullestock.net>
2018-06-30 16:03:27 +0200

    Add WebSocket test code.

--- esp32/main/main.cpp
+++ esp32/main/main.cpp
@@ -4,12 +4,18 @@
 #include <freertos/FreeRTOS.h>
 #include <freertos/queue.h>
 #include <freertos/task.h>
+
+#include <driver/ledc.h>
 #include <driver/pcnt.h>
+
+#include <lwip/api.h>
 #include <lwip/sockets.h>
+
 #include "esp_wifi.h"
 #include "sdkconfig.h"
 #include "esp_event.h"
 #include "esp_event_loop.h"
+#include "esp_log.h"
 #include "nvs_flash.h"
 
 #include "espway.h"
@@ -20,7 +26,10 @@
 #include "locks.h"
 #include "motor.h"
 
+#include "websocket_server.h"
+
 #define LED_GPIO 13
+#define LED_PIN 2 // internal
 
 #define GPIO_PWM0A_OUT 16
 #define GPIO_PWM0B_OUT 17
@@ -35,10 +44,18 @@
 #define GREEN   0, 255, 0
 #define BLUE    0, 0, 255
 
+#define PRIO_COMMUNICATION  2
+#define PRIO_MAIN_LOOP      (TCPIP_THREAD_PRIO + 1)
+
 Led* led = nullptr;
 Motor* motor_a = nullptr;
 Motor* motor_b = nullptr;
 Imu* imu = nullptr;
+
+static QueueHandle_t client_queue;
+const static int client_queue_size = 10;
+
+static ledc_channel_config_t ledc_channel;
 
 #if 0
 
@@ -308,6 +325,267 @@ static void wifi_setup()
     ESP_ERROR_CHECK(esp_wifi_start());
 }
 
+// sets up the led for pwm
+static void led_duty(uint16_t duty) {
+  static uint16_t val;
+  static uint16_t max = (1L<<10)-1;
+  if(duty > 100) return;
+  val = (duty * max) / 100;
+  ledc_set_duty(ledc_channel.speed_mode,ledc_channel.channel,val);
+  ledc_update_duty(ledc_channel.speed_mode,ledc_channel.channel);
+}
+
+static void led_setup() {
+  const static char* TAG = "led_setup";
+
+  ledc_timer_config_t ledc_timer = {
+    LEDC_HIGH_SPEED_MODE,
+    LEDC_TIMER_10_BIT,
+    LEDC_TIMER_0,
+    5000
+  };
+
+  ledc_channel.channel = LEDC_CHANNEL_0;
+  ledc_channel.duty = 0;
+  ledc_channel.gpio_num = LED_PIN,
+  ledc_channel.speed_mode = LEDC_HIGH_SPEED_MODE;
+  ledc_channel.timer_sel = LEDC_TIMER_0;
+
+  ledc_timer_config(&ledc_timer);
+  ledc_channel_config(&ledc_channel);
+  led_duty(0);
+  ESP_LOGI(TAG,"led is off and ready, 10 bits");
+}
+
+// handles websocket events
+void websocket_callback(uint8_t num,WEBSOCKET_TYPE_t type,char* msg,uint64_t len) {
+  const static char* TAG = "websocket_callback";
+  int value;
+
+  switch(type) {
+    case WEBSOCKET_CONNECT:
+      ESP_LOGI(TAG,"client %i connected!",num);
+      break;
+    case WEBSOCKET_DISCONNECT_EXTERNAL:
+      ESP_LOGI(TAG,"client %i sent a disconnect message",num);
+      led_duty(0);
+      break;
+    case WEBSOCKET_DISCONNECT_INTERNAL:
+      ESP_LOGI(TAG,"client %i was disconnected",num);
+      break;
+    case WEBSOCKET_DISCONNECT_ERROR:
+      ESP_LOGI(TAG,"client %i was disconnected due to an error",num);
+      led_duty(0);
+      break;
+    case WEBSOCKET_TEXT:
+      if(len) {
+        switch(msg[0]) {
+          case 'L':
+            if(sscanf(msg,"L%i",&value)) {
+              ESP_LOGI(TAG,"LED value: %i",value);
+              led_duty(value);
+              ws_server_send_text_all_from_callback(msg,len); // broadcast it!
+            }
+        }
+      }
+      break;
+    case WEBSOCKET_BIN:
+      ESP_LOGI(TAG,"client %i sent binary message of size %i:\n%s",num,(uint32_t)len,msg);
+      break;
+    case WEBSOCKET_PING:
+      ESP_LOGI(TAG,"client %i pinged us with message of size %i:\n%s",num,(uint32_t)len,msg);
+      break;
+    case WEBSOCKET_PONG:
+      ESP_LOGI(TAG,"client %i responded to the ping",num);
+      break;
+  }
+}
+
+// serves any clients
+static void http_serve(struct netconn *conn) {
+  const static char* TAG = "http_server";
+  const static char HTML_HEADER[] = "HTTP/1.1 200 OK\nContent-type: text/html\n\n";
+  const static char ERROR_HEADER[] = "HTTP/1.1 404 Not Found\nContent-type: text/html\n\n";
+  const static char JS_HEADER[] = "HTTP/1.1 200 OK\nContent-type: text/javascript\n\n";
+  const static char CSS_HEADER[] = "HTTP/1.1 200 OK\nContent-type: text/css\n\n";
+  //const static char PNG_HEADER[] = "HTTP/1.1 200 OK\nContent-type: image/png\n\n";
+  const static char ICO_HEADER[] = "HTTP/1.1 200 OK\nContent-type: image/x-icon\n\n";
+  //const static char PDF_HEADER[] = "HTTP/1.1 200 OK\nContent-type: application/pdf\n\n";
+  //const static char EVENT_HEADER[] = "HTTP/1.1 200 OK\nContent-Type: text/event-stream\nCache-Control: no-cache\nretry: 3000\n\n";
+  struct netbuf* inbuf;
+  static char* buf;
+  static uint16_t buflen;
+  static err_t err;
+
+  // default page
+  extern const uint8_t root_html_start[] asm("_binary_root_html_start");
+  extern const uint8_t root_html_end[] asm("_binary_root_html_end");
+  const uint32_t root_html_len = root_html_end - root_html_start;
+
+  // test.js
+  extern const uint8_t test_js_start[] asm("_binary_test_js_start");
+  extern const uint8_t test_js_end[] asm("_binary_test_js_end");
+  const uint32_t test_js_len = test_js_end - test_js_start;
+
+  // test.css
+  extern const uint8_t test_css_start[] asm("_binary_test_css_start");
+  extern const uint8_t test_css_end[] asm("_binary_test_css_end");
+  const uint32_t test_css_len = test_css_end - test_css_start;
+
+  // favicon.ico
+  extern const uint8_t favicon_ico_start[] asm("_binary_favicon_ico_start");
+  extern const uint8_t favicon_ico_end[] asm("_binary_favicon_ico_end");
+  const uint32_t favicon_ico_len = favicon_ico_end - favicon_ico_start;
+
+  // error page
+  extern const uint8_t error_html_start[] asm("_binary_error_html_start");
+  extern const uint8_t error_html_end[] asm("_binary_error_html_end");
+  const uint32_t error_html_len = error_html_end - error_html_start;
+
+  netconn_set_recvtimeout(conn,1000); // allow a connection timeout of 1 second
+  ESP_LOGI(TAG,"reading from client...");
+  err = netconn_recv(conn, &inbuf);
+  ESP_LOGI(TAG,"read from client");
+  if(err==ERR_OK) {
+    netbuf_data(inbuf, (void**)&buf, &buflen);
+    if(buf) {
+
+      // default page
+      if     (strstr(buf,"GET / ")
+          && !strstr(buf,"Upgrade: websocket")) {
+        ESP_LOGI(TAG,"Sending /");
+        netconn_write(conn, HTML_HEADER, sizeof(HTML_HEADER)-1,NETCONN_NOCOPY);
+        netconn_write(conn, root_html_start,root_html_len,NETCONN_NOCOPY);
+        netconn_close(conn);
+        netconn_delete(conn);
+        netbuf_delete(inbuf);
+      }
+
+      // default page websocket
+      else if(strstr(buf,"GET / ")
+           && strstr(buf,"Upgrade: websocket")) {
+        ESP_LOGI(TAG,"Requesting websocket on /");
+        ws_server_add_client(conn, buf, buflen, "/", websocket_callback);
+        netbuf_delete(inbuf);
+      }
+
+      else if(strstr(buf,"GET /test.js ")) {
+        ESP_LOGI(TAG,"Sending /test.js");
+        netconn_write(conn, JS_HEADER, sizeof(JS_HEADER)-1,NETCONN_NOCOPY);
+        netconn_write(conn, test_js_start, test_js_len,NETCONN_NOCOPY);
+        netconn_close(conn);
+        netconn_delete(conn);
+        netbuf_delete(inbuf);
+      }
+
+      else if(strstr(buf,"GET /test.css ")) {
+        ESP_LOGI(TAG,"Sending /test.css");
+        netconn_write(conn, CSS_HEADER, sizeof(CSS_HEADER)-1,NETCONN_NOCOPY);
+        netconn_write(conn, test_css_start, test_css_len,NETCONN_NOCOPY);
+        netconn_close(conn);
+        netconn_delete(conn);
+        netbuf_delete(inbuf);
+      }
+
+      else if(strstr(buf,"GET /favicon.ico ")) {
+        ESP_LOGI(TAG,"Sending favicon.ico");
+        netconn_write(conn,ICO_HEADER,sizeof(ICO_HEADER)-1,NETCONN_NOCOPY);
+        netconn_write(conn,favicon_ico_start,favicon_ico_len,NETCONN_NOCOPY);
+        netconn_close(conn);
+        netconn_delete(conn);
+        netbuf_delete(inbuf);
+      }
+
+      else if(strstr(buf,"GET /")) {
+        ESP_LOGI(TAG,"Unknown request, sending error page: %s",buf);
+        netconn_write(conn, ERROR_HEADER, sizeof(ERROR_HEADER)-1,NETCONN_NOCOPY);
+        netconn_write(conn, error_html_start, error_html_len,NETCONN_NOCOPY);
+        netconn_close(conn);
+        netconn_delete(conn);
+        netbuf_delete(inbuf);
+      }
+
+      else {
+        ESP_LOGI(TAG,"Unknown request");
+        netconn_close(conn);
+        netconn_delete(conn);
+        netbuf_delete(inbuf);
+      }
+    }
+    else {
+      ESP_LOGI(TAG,"Unknown request (empty?...)");
+      netconn_close(conn);
+      netconn_delete(conn);
+      netbuf_delete(inbuf);
+    }
+  }
+  else { // if err==ERR_OK
+    ESP_LOGI(TAG,"error on read, closing connection");
+    netconn_close(conn);
+    netconn_delete(conn);
+    netbuf_delete(inbuf);
+  }
+}
+
+// handles clients when they first connect. passes to a queue
+static void server_task(void* pvParameters) {
+  const static char* TAG = "server_task";
+  struct netconn *conn, *newconn;
+  static err_t err;
+  client_queue = xQueueCreate(client_queue_size,sizeof(struct netconn*));
+
+  conn = netconn_new(NETCONN_TCP);
+  netconn_bind(conn,NULL,80);
+  netconn_listen(conn);
+  ESP_LOGI(TAG,"server listening");
+  do {
+    err = netconn_accept(conn, &newconn);
+    ESP_LOGI(TAG,"new client");
+    if(err == ERR_OK) {
+      xQueueSendToBack(client_queue,&newconn,portMAX_DELAY);
+      //http_serve(newconn);
+    }
+  } while(err == ERR_OK);
+  netconn_close(conn);
+  netconn_delete(conn);
+  ESP_LOGE(TAG,"task ending, rebooting board");
+  esp_restart();
+}
+
+// receives clients from queue, handles them
+static void server_handle_task(void* pvParameters) {
+  const static char* TAG = "server_handle_task";
+  struct netconn* conn;
+  ESP_LOGI(TAG,"task starting");
+  for(;;) {
+    xQueueReceive(client_queue,&conn,portMAX_DELAY);
+    if(!conn) continue;
+    http_serve(conn);
+  }
+  vTaskDelete(NULL);
+}
+
+static void count_task(void* pvParameters) {
+  const static char* TAG = "count_task";
+  char out[20];
+  int len;
+  int clients;
+  const static char* word = "%i";
+  uint8_t n = 0;
+  const int DELAY = 1000 / portTICK_PERIOD_MS; // 1 second
+
+  ESP_LOGI(TAG,"starting task");
+  for(;;) {
+    len = sprintf(out,word,n);
+    clients = ws_server_send_text_all(out,len);
+    if(clients > 0) {
+      //ESP_LOGI(TAG,"sent: \"%s\" to %i clients",out,clients);
+    }
+    n++;
+    vTaskDelay(DELAY);
+  }
+}
+
 extern "C"
 void app_main()
 {
@@ -326,9 +604,14 @@ void app_main()
                        2.0 * 2000.0f * M_PI / 180.0f, IMU_SAMPLE_TIME);
 
     wifi_setup();
+    led_setup();
+    ws_server_start();
+    xTaskCreate(&server_task,"server_task",3000,NULL,9,NULL);
+    xTaskCreate(&server_handle_task,"server_handle_task",4000,NULL,6,NULL);
+    xTaskCreate(&count_task,"count_task",6000,NULL,2,NULL);
 
+    //!!xTaskCreate(&httpd_task, "HTTP Daemon", 256, NULL, PRIO_COMMUNICATION, NULL);
 #if 0
-    xTaskCreate(&httpd_task, "HTTP Daemon", 256, NULL, PRIO_COMMUNICATION, NULL);
     xTaskCreate(&main_loop, "Main loop", 256, NULL, PRIO_MAIN_LOOP, &xCalculationTask);
     xTaskCreate(&steering_watcher, "Steering watcher", 128, NULL, PRIO_MAIN_LOOP + 1, &xSteeringWatcher);
     xTaskCreate(&imu_watcher, "IMU watcher", 128, NULL, PRIO_MAIN_LOOP + 2, &xIMUWatcher);
@@ -354,7 +637,9 @@ void app_main()
     xTaskCreate(event_task, "event_task", 2048, &encoders, 5, NULL);
 #endif
     
+#if 0
     imu = new Imu();
 
-    xTaskCreate(main_loop, "main_loop", 10240, nullptr, 5, nullptr);
+    xTaskCreate(main_loop, "Main loop", 10240, nullptr, PRIO_MAIN_LOOP, nullptr);
+#endif
 }
