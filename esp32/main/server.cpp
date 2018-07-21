7650c176390cfc90e36bc74282dcd1e8d0f046c0
Torsten Martinsen <torsten@bullestock.net>
2018-07-21 23:43:08 +0200

    Fix indent, etc.

--- esp32/main/server.cpp
+++ esp32/main/server.cpp
@@ -169,24 +169,24 @@ void websocket_callback(uint8_t num, WEBSOCKET_TYPE_t type, char* msg, uint64_t 
     switch(type)
     {
     case WEBSOCKET_CONNECT:
-        ESP_LOGI(TAG, "client %i connected!",num);
+        ESP_LOGI(TAG, "client %i connected!", num);
         break;
     case WEBSOCKET_DISCONNECT_EXTERNAL:
-        ESP_LOGI(TAG, "client %i sent a disconnect message",num);
+        ESP_LOGI(TAG, "client %i sent a disconnect message", num);
         break;
     case WEBSOCKET_DISCONNECT_INTERNAL:
-        ESP_LOGI(TAG, "client %i was disconnected",num);
+        ESP_LOGI(TAG, "client %i was disconnected", num);
         break;
     case WEBSOCKET_DISCONNECT_ERROR:
-        ESP_LOGI(TAG, "client %i was disconnected due to an error",num);
+        ESP_LOGI(TAG, "client %i was disconnected due to an error", num);
         break;
     case WEBSOCKET_TEXT:
         if (len)
         {
             switch(msg[0]) {
             case 'L':
-                if(sscanf(msg,"L%i",&value)) {
-                    ESP_LOGI(TAG, "LED value: %i",value);
+                if(sscanf(msg, "L%i", &value)) {
+                    ESP_LOGI(TAG, "LED value: %i", value);
                     //led_duty(value);
                     ws_server_send_text_all_from_callback(msg, len); // broadcast it!
                 }
@@ -197,176 +197,176 @@ void websocket_callback(uint8_t num, WEBSOCKET_TYPE_t type, char* msg, uint64_t 
         httpd_websocket_cb((const uint8_t*) msg, len);
         break;
     case WEBSOCKET_PING:
-        ESP_LOGI(TAG, "client %i pinged us with message of size %i:\n%s",num,(uint32_t)len,msg);
+        ESP_LOGI(TAG, "client %i pinged us with message of size %i:\n%s", num, (uint32_t)len, msg);
         break;
     case WEBSOCKET_PONG:
-        ESP_LOGI(TAG, "client %i responded to the ping",num);
+        ESP_LOGI(TAG, "client %i responded to the ping", num);
         break;
     }
 }
 
 // serves any clients
 static void http_serve(struct netconn *conn) {
-  const static char* TAG = "http_server";
-  const static char HTML_HEADER[] = "HTTP/1.1 200 OK\nContent-type: text/html\n\n";
-  const static char ERROR_HEADER[] = "HTTP/1.1 404 Not Found\nContent-type: text/html\n\n";
-  const static char JS_HEADER[] = "HTTP/1.1 200 OK\nContent-type: text/javascript\n\n";
-  const static char ICO_HEADER[] = "HTTP/1.1 200 OK\nContent-type: image/x-icon\n\n";
-  struct netbuf* inbuf;
-  static char* buf;
-  static uint16_t buflen;
-  static err_t err;
+    const static char* TAG = "http_server";
+    const static char HTML_HEADER[] = "HTTP/1.1 200 OK\nContent-type: text/html\n\n";
+    const static char ERROR_HEADER[] = "HTTP/1.1 404 Not Found\nContent-type: text/html\n\n";
+    const static char JS_HEADER[] = "HTTP/1.1 200 OK\nContent-type: text/javascript\n\n";
+    const static char ICO_HEADER[] = "HTTP/1.1 200 OK\nContent-type: image/x-icon\n\n";
+    struct netbuf* inbuf;
+    static char* buf;
+    static uint16_t buflen;
+    static err_t err;
 
-  // index.html
-  extern const uint8_t index_html_start[] asm("_binary_index_html_start");
-  extern const uint8_t index_html_end[] asm("_binary_index_html_end");
-  const uint32_t index_html_len = index_html_end - index_html_start;
+    // index.html
+    extern const uint8_t index_html_start[] asm("_binary_index_html_start");
+    extern const uint8_t index_html_end[] asm("_binary_index_html_end");
+    const uint32_t index_html_len = index_html_end - index_html_start;
 
-  // index.bundle.js
-  extern const uint8_t index_bundle_js_start[] asm("_binary_index_bundle_js_start");
-  extern const uint8_t index_bundle_js_end[] asm("_binary_index_bundle_js_end");
-  const uint32_t index_bundle_js_len = index_bundle_js_end - index_bundle_js_start;
+    // index.bundle.js
+    extern const uint8_t index_bundle_js_start[] asm("_binary_index_bundle_js_start");
+    extern const uint8_t index_bundle_js_end[] asm("_binary_index_bundle_js_end");
+    const uint32_t index_bundle_js_len = index_bundle_js_end - index_bundle_js_start;
 
-  // pid.html
-  extern const uint8_t pid_html_start[] asm("_binary_pid_html_start");
-  extern const uint8_t pid_html_end[] asm("_binary_pid_html_end");
-  const uint32_t pid_html_len = pid_html_end - pid_html_start;
+    // pid.html
+    extern const uint8_t pid_html_start[] asm("_binary_pid_html_start");
+    extern const uint8_t pid_html_end[] asm("_binary_pid_html_end");
+    const uint32_t pid_html_len = pid_html_end - pid_html_start;
 
-  // pid.bundle.js
-  extern const uint8_t pid_bundle_js_start[] asm("_binary_pid_bundle_js_start");
-  extern const uint8_t pid_bundle_js_end[] asm("_binary_pid_bundle_js_end");
-  const uint32_t pid_bundle_js_len = pid_bundle_js_end - pid_bundle_js_start;
+    // pid.bundle.js
+    extern const uint8_t pid_bundle_js_start[] asm("_binary_pid_bundle_js_start");
+    extern const uint8_t pid_bundle_js_end[] asm("_binary_pid_bundle_js_end");
+    const uint32_t pid_bundle_js_len = pid_bundle_js_end - pid_bundle_js_start;
 
-  // favicon.ico
-  extern const uint8_t favicon_ico_start[] asm("_binary_favicon_ico_start");
-  extern const uint8_t favicon_ico_end[] asm("_binary_favicon_ico_end");
-  const uint32_t favicon_ico_len = favicon_ico_end - favicon_ico_start;
+    // favicon.ico
+    extern const uint8_t favicon_ico_start[] asm("_binary_favicon_ico_start");
+    extern const uint8_t favicon_ico_end[] asm("_binary_favicon_ico_end");
+    const uint32_t favicon_ico_len = favicon_ico_end - favicon_ico_start;
 
-  netconn_set_recvtimeout(conn,1000); // allow a connection timeout of 1 second
-  ESP_LOGI(TAG, "reading from client...");
-  err = netconn_recv(conn, &inbuf);
-  ESP_LOGI(TAG, "read from client");
-  if(err==ERR_OK) {
-    netbuf_data(inbuf, (void**)&buf, &buflen);
-    if(buf) {
+    netconn_set_recvtimeout(conn, 1000); // allow a connection timeout of 1 second
+    ESP_LOGI(TAG, "reading from client...");
+    err = netconn_recv(conn, &inbuf);
+    ESP_LOGI(TAG, "read from client");
+    if(err==ERR_OK) {
+        netbuf_data(inbuf, (void**)&buf, &buflen);
+        if(buf) {
 
-      // default page
-      if (strstr(buf,"GET / ")
-          && !strstr(buf,"Upgrade: websocket")) {
-        ESP_LOGI(TAG, "Sending /");
-        netconn_write(conn, HTML_HEADER, sizeof(HTML_HEADER)-1, NETCONN_NOCOPY);
-        netconn_write(conn, index_html_start, index_html_len, NETCONN_NOCOPY);
-        netconn_close(conn);
-        netconn_delete(conn);
-        netbuf_delete(inbuf);
-      }
+            // default page
+            if (strstr(buf, "GET / ")
+                && !strstr(buf, "Upgrade: websocket")) {
+                ESP_LOGI(TAG, "Sending /");
+                netconn_write(conn, HTML_HEADER, sizeof(HTML_HEADER)-1, NETCONN_NOCOPY);
+                netconn_write(conn, index_html_start, index_html_len, NETCONN_NOCOPY);
+                netconn_close(conn);
+                netconn_delete(conn);
+                netbuf_delete(inbuf);
+            }
 
-      // default page websocket
-      else if((strstr(buf,"GET / ") || strstr(buf,"GET /ws "))
-              && strstr(buf,"Upgrade: websocket")) {
-        ESP_LOGI(TAG, "Requesting websocket on /");
-        ws_server_add_client(conn, buf, buflen, "/", websocket_callback);
-        netbuf_delete(inbuf);
-      }
+            // default page websocket
+            else if((strstr(buf, "GET / ") || strstr(buf, "GET /ws "))
+                    && strstr(buf, "Upgrade: websocket")) {
+                ESP_LOGI(TAG, "Requesting websocket on /");
+                ws_server_add_client(conn, buf, buflen, "/", websocket_callback);
+                netbuf_delete(inbuf);
+            }
 
-      else if (strstr(buf,"GET /pid") && !strstr(buf,"GET /pid.bundle.js")) {
-        ESP_LOGI(TAG, "Sending /pid.html");
-        netconn_write(conn, HTML_HEADER, sizeof(HTML_HEADER)-1, NETCONN_NOCOPY);
-        netconn_write(conn, pid_html_start, pid_html_len, NETCONN_NOCOPY);
-        netconn_close(conn);
-        netconn_delete(conn);
-        netbuf_delete(inbuf);
-      }
-      else if(strstr(buf,"GET /index.bundle.js ")) {
-        ESP_LOGI(TAG, "Sending /index.bundle.js");
-        netconn_write(conn, JS_HEADER, sizeof(JS_HEADER)-1, NETCONN_NOCOPY);
-        netconn_write(conn, index_bundle_js_start, index_bundle_js_len, NETCONN_NOCOPY);
-        netconn_close(conn);
-        netconn_delete(conn);
-        netbuf_delete(inbuf);
-      }
-      else if(strstr(buf,"GET /pid.bundle.js ")) {
-        ESP_LOGI(TAG, "Sending /pid.bundle.js");
-        netconn_write(conn, JS_HEADER, sizeof(JS_HEADER)-1, NETCONN_NOCOPY);
-        netconn_write(conn, pid_bundle_js_start, pid_bundle_js_len, NETCONN_NOCOPY);
-        netconn_close(conn);
-        netconn_delete(conn);
-        netbuf_delete(inbuf);
-      }
+            else if (strstr(buf, "GET /pid") && !strstr(buf, "GET /pid.bundle.js")) {
+                ESP_LOGI(TAG, "Sending /pid.html");
+                netconn_write(conn, HTML_HEADER, sizeof(HTML_HEADER)-1, NETCONN_NOCOPY);
+                netconn_write(conn, pid_html_start, pid_html_len, NETCONN_NOCOPY);
+                netconn_close(conn);
+                netconn_delete(conn);
+                netbuf_delete(inbuf);
+            }
+            else if(strstr(buf, "GET /index.bundle.js ")) {
+                ESP_LOGI(TAG, "Sending /index.bundle.js");
+                netconn_write(conn, JS_HEADER, sizeof(JS_HEADER)-1, NETCONN_NOCOPY);
+                netconn_write(conn, index_bundle_js_start, index_bundle_js_len, NETCONN_NOCOPY);
+                netconn_close(conn);
+                netconn_delete(conn);
+                netbuf_delete(inbuf);
+            }
+            else if(strstr(buf, "GET /pid.bundle.js ")) {
+                ESP_LOGI(TAG, "Sending /pid.bundle.js");
+                netconn_write(conn, JS_HEADER, sizeof(JS_HEADER)-1, NETCONN_NOCOPY);
+                netconn_write(conn, pid_bundle_js_start, pid_bundle_js_len, NETCONN_NOCOPY);
+                netconn_close(conn);
+                netconn_delete(conn);
+                netbuf_delete(inbuf);
+            }
 
-      else if(strstr(buf,"GET /favicon.ico ")) {
-        ESP_LOGI(TAG, "Sending favicon.ico");
-        netconn_write(conn,ICO_HEADER,sizeof(ICO_HEADER)-1, NETCONN_NOCOPY);
-        netconn_write(conn,favicon_ico_start,favicon_ico_len, NETCONN_NOCOPY);
-        netconn_close(conn);
-        netconn_delete(conn);
-        netbuf_delete(inbuf);
-      }
+            else if(strstr(buf, "GET /favicon.ico ")) {
+                ESP_LOGI(TAG, "Sending favicon.ico");
+                netconn_write(conn, ICO_HEADER, sizeof(ICO_HEADER)-1, NETCONN_NOCOPY);
+                netconn_write(conn, favicon_ico_start, favicon_ico_len, NETCONN_NOCOPY);
+                netconn_close(conn);
+                netconn_delete(conn);
+                netbuf_delete(inbuf);
+            }
 
-      else if(strstr(buf,"GET /")) {
-        ESP_LOGI(TAG, "Unknown GET request: %s", buf);
-        netconn_write(conn, ERROR_HEADER, sizeof(ERROR_HEADER)-1, NETCONN_NOCOPY);
-        netconn_close(conn);
-        netconn_delete(conn);
-        netbuf_delete(inbuf);
-      }
+            else if(strstr(buf, "GET /")) {
+                ESP_LOGI(TAG, "Unknown GET request: %s", buf);
+                netconn_write(conn, ERROR_HEADER, sizeof(ERROR_HEADER)-1, NETCONN_NOCOPY);
+                netconn_close(conn);
+                netconn_delete(conn);
+                netbuf_delete(inbuf);
+            }
 
-      else {
-          ESP_LOGI(TAG, "Unknown request: %s", buf);
-        netconn_close(conn);
-        netconn_delete(conn);
-        netbuf_delete(inbuf);
-      }
+            else {
+                ESP_LOGI(TAG, "Unknown request: %s", buf);
+                netconn_close(conn);
+                netconn_delete(conn);
+                netbuf_delete(inbuf);
+            }
+        }
+        else {
+            ESP_LOGI(TAG, "Unknown request (empty?...)");
+            netconn_close(conn);
+            netconn_delete(conn);
+            netbuf_delete(inbuf);
+        }
     }
-    else {
-      ESP_LOGI(TAG, "Unknown request (empty?...)");
-      netconn_close(conn);
-      netconn_delete(conn);
-      netbuf_delete(inbuf);
+    else { // if err==ERR_OK
+        ESP_LOGI(TAG, "error on read, closing connection");
+        netconn_close(conn);
+        netconn_delete(conn);
+        netbuf_delete(inbuf);
     }
-  }
-  else { // if err==ERR_OK
-    ESP_LOGI(TAG, "error on read, closing connection");
-    netconn_close(conn);
-    netconn_delete(conn);
-    netbuf_delete(inbuf);
-  }
 }
 
 // handles clients when they first connect. passes to a queue
 void server_task(void* pvParameters) {
-  const static char* TAG = "server_task";
-  struct netconn *conn, *newconn;
-  static err_t err;
-  client_queue = xQueueCreate(client_queue_size,sizeof(struct netconn*));
+    const static char* TAG = "server_task";
+    struct netconn *conn, *newconn;
+    static err_t err;
+    client_queue = xQueueCreate(client_queue_size, sizeof(struct netconn*));
 
-  conn = netconn_new(NETCONN_TCP);
-  netconn_bind(conn, NULL,80);
-  netconn_listen(conn);
-  ESP_LOGI(TAG, "server listening");
-  do {
-    err = netconn_accept(conn, &newconn);
-    ESP_LOGI(TAG, "new client");
-    if(err == ERR_OK) {
-      xQueueSendToBack(client_queue, &newconn, portMAX_DELAY);
-      //http_serve(newconn);
-    }
-  } while(err == ERR_OK);
-  netconn_close(conn);
-  netconn_delete(conn);
-  ESP_LOGE(TAG, "task ending, rebooting board");
-  esp_restart();
+    conn = netconn_new(NETCONN_TCP);
+    netconn_bind(conn, NULL,80);
+    netconn_listen(conn);
+    ESP_LOGI(TAG, "server listening");
+    do {
+        err = netconn_accept(conn, &newconn);
+        ESP_LOGI(TAG, "new client");
+        if (err == ERR_OK)
+            xQueueSendToBack(client_queue, &newconn, portMAX_DELAY);
+    } while(err == ERR_OK);
+    netconn_close(conn);
+    netconn_delete(conn);
+    ESP_LOGE(TAG, "task ending, rebooting board");
+    esp_restart();
 }
 
 // receives clients from queue, handles them
 void server_handle_task(void* pvParameters) {
-  const static char* TAG = "server_handle_task";
-  struct netconn* conn;
-  ESP_LOGI(TAG, "task starting");
-  for(;;) {
-    xQueueReceive(client_queue,&conn,portMAX_DELAY);
-    if(!conn) continue;
-    http_serve(conn);
-  }
-  vTaskDelete(NULL);
+    const static char* TAG = "server_handle_task";
+    struct netconn* conn;
+    ESP_LOGI(TAG, "task starting");
+    while (1)
+    {
+        xQueueReceive(client_queue,&conn,portMAX_DELAY);
+        if (!conn)
+            continue;
+        http_serve(conn);
+    }
+    vTaskDelete(NULL);
 }
