310e9a50e65e3dc31cd589ef6dc359c630332341
Torsten Martinsen <torsten@bullestock.net>
2021-05-15 15:43:02 +0200

    Fix bugs.

--- manage.py
+++ manage.py
@@ -27,18 +27,40 @@ RES_SET_GYRO_OFFSETS_SUCCESS = 11
 REQ_SAVE_CONFIG = 12
 RES_SAVE_CONFIG_FAILURE = 13
 RES_SAVE_CONFIG_SUCCESS = 14
+REQ_GET_STATS = 22
+RES_GET_STATS = 23
+REQ_GET_STATUS = 24
+RES_GET_STATUS = 25
+
 Q16_ONE = 65536
 
+# Get a Q16 float from the 4 bytes starting at 'start'
 def get_q16(ba, start):
     sv = ba[start] + 256*ba[start+1] + 256*256*ba[start+2] + 256*256*256*ba[start+3]
     return sv/Q16_ONE
 
+# Store a Q16 float into the 4 bytes starting at 'start'
 def set_q16(ba, val):
     sv = int(val*Q16_ONE)
     ba.append(sv & 255)
     ba.append((sv // 256) & 255)
     ba.append((sv // (256*256)) & 255)
     ba.append((sv // (256*256*256)) & 255)
+
+def get_uint32(ba, start):
+    return ba[start] + 256*ba[start+1] + 256*256*ba[start+2] + 256*256*256*ba[start+3]
+
+def get_state(ba, start):
+    state = ba[start]
+    if state == 0:
+        return 'Stabilizing'
+    if state == 1:
+        return 'Running'
+    if state == 2:
+        return 'Fallen'
+    if state == 3:
+        return 'Wound up'
+    return 'Unknown state: %d' % state
 
 def on_message(ws, message):
     ba = bytearray(message)
@@ -55,9 +77,11 @@ def on_message(ws, message):
             ws.cur_params = [ cur_params ]
         if pid_idx == 1:
             ws.cur_params.append(cur_params)
-            if ws.state == 'WAIT_GET':
+            if ws.state == 'WAIT_GET_PID':
                 if not ws.set_params:
-                    ws.close()
+                    bytes = [REQ_GET_STATS]
+                    ws.sock.send_binary(bytearray(bytes))
+                    ws.state == 'WAIT_GET_STATS'
                 else:
                     print("Setting PID parameters")
                     try:
@@ -69,7 +93,6 @@ def on_message(ws, message):
                                 if p != None:
                                     param = p
                             set_q16(bytes, param)
-                        #print(bytes)
                         ws.sock.send_binary(bytearray(bytes))
                         ws.state = 'WAIT_SET'
                     except Exception as e:
@@ -77,12 +100,19 @@ def on_message(ws, message):
             elif ws.state == 'WAIT_GET_VERIFY':
                 print('All done')
                 ws.close()
-
     elif opcode == RES_SET_PID_PARAMS_ACK:
         print("Verifying PID parameters")
         ws.sock.send_binary(bytearray([REQ_GET_PID_PARAMS, 0]))
         ws.sock.send_binary(bytearray([REQ_GET_PID_PARAMS, 1]))
         ws.state = 'WAIT_GET_VERIFY'
+    elif opcode == RES_GET_STATS:
+        print("Looptime %u" % get_uint32(ba, 1))
+        bytes = [REQ_GET_STATUS]
+        ws.sock.send_binary(bytearray(bytes))
+        ws.state == 'WAIT_GET_STATUS'
+    elif opcode == RES_GET_STATUS:
+        print("State %s" % get_state(ba, 1))
+        ws.state = 'IDLE'
 
 def on_error(ws, error):
     print(error)
@@ -96,13 +126,21 @@ def on_open(ws):
     # Steering: '0' <velocity> <turn rate>
     def run(*args):
         ws.state = 'INIT'
+        ws.last_update = time.time()
         while True:
             time.sleep(0.1)
             if ws.state == 'INIT':
                 print("Requesting PID parameters")
                 ws.sock.send_binary(bytearray([REQ_GET_PID_PARAMS, 0]))
                 ws.sock.send_binary(bytearray([REQ_GET_PID_PARAMS, 1]))
-                ws.state = 'WAIT_GET'
+                ws.state = 'WAIT_GET_PID'
+            elif ws.state == 'IDLE':
+                if time.time() - ws.last_update > 10:
+                    ws.last_update = time.time()
+                    bytes = [REQ_GET_STATS]
+                    ws.sock.send_binary(bytearray(bytes))
+                    ws.state == 'WAIT_GET_STATS'
+
     thread.start_new_thread(run, ())
 
 if __name__ == "__main__":
@@ -136,7 +174,6 @@ if __name__ == "__main__":
 
     print("ESP: Connected")
             
-    #websocket.enableTrace(True)
     print("Connecting to ESPWay websocket")
     ws = websocket.WebSocketApp("ws://10.0.0.1/",
                                 on_open = on_open,
